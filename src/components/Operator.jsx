be458bd5a4dd2804df7bcbbc2c6a96116861778c
afw821 <afw821@gmail.com>
2020-10-14 19:01:31 -0400

    newest commit

--- src/components/Operator.jsx
+++ src/components/Operator.jsx
@@ -4,7 +4,9 @@ const Operator = ({ value, operator, handleSetOperator, isClear }) => {
   return (
     <button
       id={`button-${value}`}
-      className={`btn btn-danger ${isClear ? "" : "btn-width"} operator`}
+      className={`btn ${
+        isClear ? "btn-dark" : "btn-width btn-danger"
+      } operator`}
       value={value}
       onClick={(e) => handleSetOperator(e)}
     >
