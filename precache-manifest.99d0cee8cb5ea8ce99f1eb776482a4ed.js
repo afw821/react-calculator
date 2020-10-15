ae4195224c7570cbf26e28bd202094eeca5ed094
Alex Watkins <afw821@gmail.com>
2020-10-14 21:26:19 -0400

    Updates

---
+++ precache-manifest.99d0cee8cb5ea8ce99f1eb776482a4ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78653592e25a305795cddad3e47f0899",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "dc94aa304684c7fa2e89",
+    "url": "/react-calculator/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "223fa3de2716b04997c1",
+    "url": "/react-calculator/static/css/main.605556e0.chunk.css"
+  },
+  {
+    "revision": "dc94aa304684c7fa2e89",
+    "url": "/react-calculator/static/js/2.e0779ce4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.e0779ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223fa3de2716b04997c1",
+    "url": "/react-calculator/static/js/main.2ead4189.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
